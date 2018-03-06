af750b4a64593310e7c875343d3f1d3cd043a913
vicanso <vicansocanbico@gmail.com>
2018-03-06 21:50:50 +0800

    feat: 增加tiny功能

--- site/src/main.js
+++ site/src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 import App from './App.vue'
+
+Vue.use(Element)
 
 Vue.config.productionTip = false
 
---
+++ site/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5018',
+      },
+    },
+  },
+};
