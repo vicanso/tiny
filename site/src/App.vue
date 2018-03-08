8c2743b8c7f347f182fe0bc856182ce65748bb88
vicanso <vicansocanbico@gmail.com>
2018-03-08 20:08:04 +0800

    refactor: 删除`font-smoothing`

--- site/src/App.vue
+++ site/src/App.vue
@@ -185,8 +185,6 @@ export default {
 }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
